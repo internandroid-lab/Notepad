5d8cd80b97d3650d1ef4e2a2c2d5ad7a2cec2a2a
internandroid-lab <internandroid@lutech.ltd>
2025-09-03 16:57:51 +0700

    update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,22 +57,19 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
     implementation("io.coil-kt:coil-compose:2.5.0")
 
-    // Room dependencies
     implementation("androidx.room:room-runtime:2.7.2")
     implementation("androidx.room:room-ktx:2.7.2")
     kapt("androidx.room:room-compiler:2.7.2")
 
-    // Navigation dependencies
     implementation("androidx.navigation:navigation-fragment-ktx:2.8.4")
     implementation("androidx.navigation:navigation-ui-ktx:2.8.4")
 
-    // ViewModel and LiveData
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
     implementation("androidx.fragment:fragment-ktx:1.8.5")
 
-//    implementation("io.insert-koin:koin-core:3.5.3")
-//    implementation("io.insert-koin:koin-android:3.5.3")
-//    implementation("io.insert-koin:koin-androidx-viewmodel:3.5.3")
+    implementation("io.insert-koin:koin-core:3.5.3")
+    implementation("io.insert-koin:koin-android:3.5.3")
+    implementation("io.insert-koin:koin-androidx-viewmodel:3.5.3")
 
 }
